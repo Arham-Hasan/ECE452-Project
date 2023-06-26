e2be9378b15db1dda02934b0331e816db745ff0d
Umar Asif <umarasifs@gmail.com>
2023-06-26 11:56:57 -0400

    fixed login

--- app/src/main/java/com/example/tracktor/screens/pickingmode/PickingModeViewModel.kt
+++ app/src/main/java/com/example/tracktor/screens/pickingmode/PickingModeViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.tracktor.screens.pickingmode
 
+import com.example.tracktor.common.snackbar.SnackbarManager
+import com.example.tracktor.common.snackbar.SnackbarMessage.Companion.toSnackbarMessage
 import com.example.tracktor.model.service.AccountService
 import com.example.tracktor.screens.TracktorViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -8,6 +10,6 @@ import javax.inject.Inject
 @HiltViewModel
 class PickingModeViewModel @Inject constructor() : TracktorViewModel() {
     fun onMicButtonClick(){
-        println("Listening")
+        SnackbarManager.showMessage("Listening".toSnackbarMessage())
     }
 }

5f5b30bf684c0632405ec82a7a5ac3ee307742fa
Arham-Hasan <a39hasan@uwaterloo.ca>
2023-06-26 01:56:54 -0400

    farm select screen skeleton

--- app/src/main/java/com/example/tracktor/screens/selectfarm/SelectFarmScreen.kt
+++ app/src/main/java/com/example/tracktor/screens/selectfarm/SelectFarmScreen.kt
@@ -1,10 +1,13 @@
 package com.example.tracktor.screens.selectfarm
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -14,13 +17,17 @@ import com.example.tracktor.model.Farm
 fun SelectFarmScreen(openAndPopUp: (String, String) -> Unit, openScreen: (String) -> Unit, viewModel: SelectFarmViewModel = hiltViewModel()) {
     val farms: List<Farm> = viewModel.farms
 
-    Column {
+    Column(
+        Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         farms.forEach { item ->
             Text(
                 text = item.name,
                 modifier = Modifier
                     .padding(8.dp)
                     .clickable{}
+                    .fillMaxSize()
             )
         }
     }

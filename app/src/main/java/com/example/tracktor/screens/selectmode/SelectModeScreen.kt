f153d4ef268d01b279c40d67c4987005e05f0d21
yousefsaid <y3said@uwaterloo.ca>
2023-06-26 00:53:49 -0400

    Added picking screen, created pickingmodeviewmodel file, edited selectmodescreen (Pair program with Akbar)

--- app/src/main/java/com/example/tracktor/screens/selectmode/SelectModeScreen.kt
+++ app/src/main/java/com/example/tracktor/screens/selectmode/SelectModeScreen.kt
@@ -13,11 +13,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.tracktor.PICKING_MODE_SCREEN
+import com.example.tracktor.SIGN_UP_SCREEN
+import com.example.tracktor.TracktorAppState
 
 @Composable
-fun SelectModeScreen() {
+fun SelectModeScreen(navigate: (String) -> Unit) {
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
@@ -28,7 +32,7 @@ fun SelectModeScreen() {
             verticalArrangement = Arrangement.Center
         ) {
             Button(
-                onClick = {},
+                onClick = {navigate(PICKING_MODE_SCREEN)},
                 modifier = Modifier.padding(8.dp)
             ) {
                 Text(text = "Farming")
@@ -50,4 +54,5 @@ fun SelectModeScreen() {
         }
     }
 }
+
 

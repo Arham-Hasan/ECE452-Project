7265fdc7ba6931ea7425181e5d590f83ed4f7233
Akbar Zafar <akbarzafar2001@gmail.com>
2023-06-27 15:14:32 -0400

    mic input works

--- app/src/main/java/com/example/tracktor/TracktorActivity.kt
+++ app/src/main/java/com/example/tracktor/TracktorActivity.kt
@@ -1,16 +1,55 @@
 package com.example.tracktor
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.mutableStateOf
+import com.example.tracktor.common.snackbar.SnackbarManager
+import com.example.tracktor.common.snackbar.SnackbarMessage.Companion.toSnackbarMessage
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Locale
 
 @AndroidEntryPoint
 class TracktorActivity : ComponentActivity() {
+    var speechInput = mutableStateOf("")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TracktorApp()
+        }
+    }
+
+    fun onMicButtonClick(context: Context){
+        SnackbarManager.showMessage("Listening".toSnackbarMessage())
+
+        if (!SpeechRecognizer.isRecognitionAvailable(context)) {
+            Toast.makeText(context, "Speech not Available", Toast.LENGTH_SHORT).show()
+        } else {
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            intent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH
+            )
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Talk")
+            startActivityForResult(intent, 102)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 102 && resultCode == Activity.RESULT_OK) {
+            val result = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            speechInput.value = result?.get(0).toString()
+            SnackbarManager.showMessage(speechInput.value.toSnackbarMessage())
         }
     }
 }
--- app/src/main/java/com/example/tracktor/screens/pickingmode/PickingModeViewModel.kt
+++ app/src/main/java/com/example/tracktor/screens/pickingmode/PickingModeViewModel.kt
@@ -4,13 +4,12 @@ import com.example.tracktor.ANALYTICS_MODE_SCREEN
 import com.example.tracktor.FRIDGE_MODE_SCREEN
 import com.example.tracktor.INVENTORY_MODE_SCREEN
 import com.example.tracktor.SELLING_MODE_SCREEN
-import com.example.tracktor.SIGN_UP_SCREEN
 import com.example.tracktor.common.snackbar.SnackbarManager
 import com.example.tracktor.common.snackbar.SnackbarMessage.Companion.toSnackbarMessage
-import com.example.tracktor.model.service.AccountService
 import com.example.tracktor.screens.TracktorViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+
 
 @HiltViewModel
 class PickingModeViewModel @Inject constructor() : TracktorViewModel() {

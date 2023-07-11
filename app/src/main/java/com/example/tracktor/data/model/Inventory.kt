1d258418ac8d7e5b5bafaf73fa60dca355a6a941
Jasraj Bedi <jazzy@hacker.af>
2023-07-11 19:04:11 -0400

    inventory repo ops

--- app/src/main/java/com/example/tracktor/data/model/Inventory.kt
+++ app/src/main/java/com/example/tracktor/data/model/Inventory.kt
@@ -1,6 +1,5 @@
 package com.example.tracktor.data.model
 
 data class Inventory (
-    val id : String = "",
     val itemMap: MutableMap<String,InventoryItem> = mutableMapOf<String,InventoryItem>()
         )
--- app/src/main/java/com/example/tracktor/data/repository/InventoryRepository.kt
+++ app/src/main/java/com/example/tracktor/data/repository/InventoryRepository.kt
@@ -1,5 +1,16 @@
 package com.example.tracktor.data.repository
 
+import com.google.firebase.firestore.DocumentSnapshot
+
 interface InventoryRepository {
     suspend fun createInventory(id: String) : Unit
+    suspend fun getInventoryById(id: String) : DocumentSnapshot
+    suspend fun itemExists(name : String, inventoryId: String) : Boolean
+    suspend fun addItem(name : String, inventoryId: String) : Unit
+
+    suspend fun userStatExistsForItem(itemName : String, userId : String, inventoryId: String) : Boolean
+
+    suspend fun addUserStatForItem(itemName : String, userId : String, inventoryId: String) : Unit
+
+
 }

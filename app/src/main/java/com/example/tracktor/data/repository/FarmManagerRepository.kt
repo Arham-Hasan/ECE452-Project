7eebd62a8f8ead98e2c08be5020b15aee0f702d5
Arham-Hasan <a39hasan@uwaterloo.ca>
2023-07-12 17:24:37 -0400

    user request ops

--- app/src/main/java/com/example/tracktor/data/repository/FarmManagerRepository.kt
+++ app/src/main/java/com/example/tracktor/data/repository/FarmManagerRepository.kt
@@ -5,16 +5,13 @@ import com.example.tracktor.data.model.Farm
 interface FarmManagerRepository {
 
     fun changeSelectedFarm(farm:Farm): Unit
-
     fun removeSelectedFarm(): Unit
     fun getSelectedFarm(): Farm?
-
     suspend fun getActiveFarms() : List<Farm?>
     suspend fun createFarm(name: String) : Unit
-
     suspend fun deleteFarm(): Unit
-
     suspend fun requestToJoinFarm(farmId: String): Unit
-
+    suspend fun acceptUserRequest(userId:String): Unit
+    suspend fun declineUserRequest(userId: String): Unit
     suspend fun isAdmin():Boolean
 }

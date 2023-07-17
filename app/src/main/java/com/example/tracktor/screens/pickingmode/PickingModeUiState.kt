a954820e76694f888e49747c070c261d25b44384
Hammad Khan <20hammad01@gmail.com>
2023-07-17 17:06:58 -0400

    when we leave the picking page, the session is recorded

--- app/src/main/java/com/example/tracktor/screens/pickingmode/PickingModeUiState.kt
+++ app/src/main/java/com/example/tracktor/screens/pickingmode/PickingModeUiState.kt
@@ -2,5 +2,6 @@ package com.example.tracktor.screens.pickingmode
 
 data class PickingModeUiState (
     val dropDrownExtended: Boolean = false,
-    val validItems: Set<String> = setOf()
+    val validItems: Set<String> = setOf(),
+    val transactions: MutableMap<String, Int> = mutableMapOf<String, Int>()
 )
